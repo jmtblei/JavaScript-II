dcbcde4b2a4af3d400732bd7e9bd14cf9878ffb2
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-02-26 16:09:32 -0800

    commit

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,66 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(function(cv) {
+    return fullName.push(cv.first_name + " " + cv.last_name);
+});
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+runners.map(function(cv) {
+    return allCaps.push((cv.first_name).toUpperCase());
+});
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+runners.filter(function(cv) {
+    if (cv.shirt_size === "L") {
+        return largeShirts.push(cv);
+    } else {
+
+    };
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal.push(runners.reduce(function(added, cv) {
+    return added + cv.donation;
+}, 0));
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// Use .filter() to get a list of runners' full names and id# with donations that exceed 200.
+let topdonators = [];
+runners.filter(function(cv) {
+    if (cv.donation > 200) {
+        return topdonators.push(cv.id + " " + cv.first_name + " " + cv.last_name);
+    } else {
+
+    };
+});
+console.log(topdonators);
 
 // Problem 2
+// Use .forEach() to create a new array that contains each runner's first name-company name.
+let companywars = [];
+runners.forEach(function(cv) {
+    return companywars.push(cv.first_name + "-" + cv.company_name);
+});
+console.log(companywars);
 
 // Problem 3
+//  Use .map() to add 999 to all runners' donation amounts
+let fakedonos = [];
+runners.map(function(cv) {
+    return fakedonos.push(cv.donation + 999);
+});
+console.log(fakedonos);
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,71 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function thanks() {
+  const a = "friend";
+  console.log(`Thanks ${a}!`);
+  // debugger;
 
+  function reply() {
+    const b = "buddy";
+    console.log(`I'm not your  ${a}, ${b}.`)
+    // debugger;
+
+    function smarty() {
+      const c = "pal";
+      console.log(`I'm not your ${b}. ${c}.`);
+      // debugger;
+
+      function okkk() {
+        const d = "guy";
+        console.log(`I'm not your ${c}, ${d}.`);
+        // debugger;
+      }
+
+      okkk();
+    }
+
+    smarty();
+  }
+
+  reply();
+}
+
+thanks();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = (i = 0) => any = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  i++;
+  return console.log(i);
 };
+const newCounter = counter();
+newCounter();
+newCounter();
+newCounter();
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = (i = 0) => any = {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  increment: () => {
+    i++;
+    return console.log(i);
+  },
+  decrement: () => {
+    i--;
+    return console.log(i);
+  }
 };
+const moreorless = counterFactory();
+moreorless.increment();
+moreorless.increment();
+moreorless.decrement();
+moreorless.increment();
